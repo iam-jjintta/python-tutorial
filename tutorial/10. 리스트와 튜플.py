5361c61eb5806649c06debb4e5d81754cbe25799
흔한 찐따 <100037482+iam-jjintta@users.noreply.github.com>
2022-02-23 00:17:04 +0900

    10. 리스트와 튜플
    수정 - 흔한 찐따의 리스트와 튜플 공부하기 (에러 내용 추가)

--- tutorial/10. 리스트와 튜플.py
+++ tutorial/10. 리스트와 튜플.py
@@ -9,6 +9,7 @@ print('t:', t)
 l[0] = 4
 print('l:', l)
 
-# 튜플은 값을 변경할 수 없기 때문에 에러가 발생한다.
+# 튜플은 값을 변경할 수 없기 때문에 아래처럼 에러가 발생한다.
+# TypeError: 'tuple' object does not support item assignment
 t[0] = 4
 print('t:', t)
