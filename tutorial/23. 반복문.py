2efa077c3401772fdfe7c907d61cad33aea12316
흔한 찐따 <100037482+iam-jjintta@users.noreply.github.com>
2022-02-25 16:48:14 +0900

    23. 반복문
    흔한 찐따의 파이썬 반복문 사용 해보기

--- tutorial/23. 반복문.py
+++ tutorial/23. 반복문.py
@@ -1,5 +1,14 @@
 
+# while 문
+a = 1
+while a <= 10:
+    a = a + 1
+    print('a:', a)
+else:
+    print('done!')
+
+# for 문
 for i in range(1, 10):
-    print(i)
+    print('i:', i)
 else:
     print('done!')
