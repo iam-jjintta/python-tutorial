89ac41613c6ddce517629186b0293009d925d94a
흔한 찐따 <100037482+iam-jjintta@users.noreply.github.com>
2022-03-12 14:16:33 +0900

    39. 프로그래밍 명명 규칙과 표기법
    흔한 찐따의 파이썬 프로그래밍 명명 규칙과 표기법 알아보기

---
+++ tutorial/39. 프로그래밍 명명 규칙과 표기법.py
@@ -0,0 +1,30 @@
+
+# 카멜 표기법
+def lifeIsShort():
+    return 'Life is short'
+
+myMessage = lifeIsShort()
+print(myMessage)
+
+# 파스칼 표기법
+def LifeIsShort():
+    return 'Life is short'
+
+MyMessage = LifeIsShort()
+print(MyMessage)
+
+# 헝가리안 표기법
+intNumber = 10
+floatNumber = 3.14
+strMessage = 'Life is short'
+
+print(intNumber)
+print(floatNumber)
+print(strMessage)
+
+# 스네이크 표기법
+def life_is_short():
+    return 'Life is short'
+
+my_message = life_is_short()
+print(my_message)
