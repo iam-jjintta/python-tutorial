554304de394b1e41e1d8c6f008e442b5b72e041e
흔한 찐따 <100037482+iam-jjintta@users.noreply.github.com>
2022-04-08 18:16:47 +0900

    흔한 찐따의 파이썬 예제 코드 - requests와 bs4
    흔한 찐따의 파이썬 예제 코드 - requests 라이브러리와 bs4 라이브러리를 통해 구글 로고 이미지 가져오기

--- projects/sample-codes/07. google_logo_using_requests_and_bs4.py
+++ projects/sample-codes/07. google_logo_using_requests_and_bs4.py
@@ -15,13 +15,25 @@ from bs4 import BeautifulSoup
 
 url = 'https://www.google.com'
 google = requests.get(url)
+html = google.text
 
-# lxml 패키지에 있는 파서(parser)를 사용할 수 있다.
-parser = BeautifulSoup(google.text, 'lxml')
+# html.parser 라이브러리와 html5lib, 그리고 lxml 라이브러리에 있는 파서(parser)들을 사용할 수 있다.
+# 가능하다면, 속도를 위해 lxml 라이브러리를 설치해 사용하는 것을 권장한다고 한다.
+# 구버전의 파이썬을 사용한다면 lxml 라이브러리를 사용하는 것이 필수라고 한다.
+# 그렇지 않고 구버전의 파이썬 내장 HTML 해석기 html5lib 라이브러리는 별로 좋지 않다고 한다.
+# 다만, 복잡한 HTML 문서같은 경우(특히 HTML 문서의 태그가 엉망인 경우)에는 html.parser 혹은 html5lib 라이브러리를 고려해볼만 하다.
+# 즉, 결론은 lxml 라이브러리는 속도와 성능면에서 굉장히 우수하기 때문에 lxml 라이브러리를 사용하는 것이 좋다.
+parser = BeautifulSoup(html, 'lxml')
 
 # lxml과 마찬가지로 XPath 혹은 CSS Selector 문법을 통해 태그를 가져올 수 있다.
-# CSS Selector 문법은 XPath보다 성능이 더 좋다고 한다.
-img_tags = parser.findall('img')
+# CSS Selector 문법은 XPath보다 훨씬 더 쉽고 속도와 성능이 더 뛰어나다고 한다.
+# 그러나 CSS Selector는 태그의 속성으로만 검색이 가능하다.
+# 따라서 단순히 HTML 문서에서 텍스트만으로 태그를 검색하려면 XPath를 사용해야 한다.
+# 하지만 대부분의 HTML 문서에는 태그의 id를 사용하는 것이 일반적이다.
+# 고로 CSS Selector를 사용하는 편이 문법적으로도 쉽고 성능도 더 좋기 때문에 XPath보다 더 권장되는 방식이다.
+# CSS Selector를 사용하여 태그를 찾기 위해서는 'select' 메서드나 'find_all' 메서드를 사용하면 된다.
+# 태그를 하나만 가져오려면 'select_one' 메서드나 'find' 메서드를 통해 가져올 수 있다.
+img_tags = parser.find_all('img')
 
 for tag in img_tags:
     # dict 타입처럼 Key 인덱싱을 통해 태그의 속성값을 가져올 수 있다.
