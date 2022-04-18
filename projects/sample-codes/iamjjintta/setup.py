f5df5308f9696b1900cd1c06dac8c1196f5b80e6
흔한 찐따 <100037482+iam-jjintta@users.noreply.github.com>
2022-04-18 18:52:16 +0900

    흔한 찐따의 파이썬 예제 코드 - PyPI에 패키지 베포하기
    흔한 찐따의 파이썬 예제 코드 - PyPI에 패키지 베포하기

---
+++ projects/sample-codes/iamjjintta/setup.py
@@ -0,0 +1,31 @@
+
+from setuptools import setup, find_packages
+from setuptools import find_packages, setup
+
+setup(
+    name             = 'iamjjintta',
+    version          = '0.0.1',
+    description      = '나는 찐따다.',
+    author           = 'iam-jjintta',
+    author_email     = 'iamjjintta@gmail.com',
+    url              = 'https://github.com/iam-jjintta',
+    download_url     = 'https://github.com/iam-jjintta',
+    install_requires = [ ],
+    packages= find_packages(),
+    keywords         = ['iamjjintta'],
+    python_requires  = '>=3',
+    zip_safe=False,
+    classifiers      = [
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.1',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+    ]
+)
